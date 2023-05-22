ba7c76ed3d17513cf9286d94a289b22d79f1cf7a
pavanemrsoft <100695467+pavanemrsoft@users.noreply.github.com>
2023-05-22 15:31:12 +0530

    Update daogen version

--- src/DaoGen.php
+++ src/DaoGen.php
@@ -11,7 +11,7 @@
 
 ##############################################################################################################
 
-$daoGenVersion = '0.5.22';
+$daoGenVersion = '0.5.23';
 
 require_once 'includes/includes.php';
 require_once 'class.database.php';
